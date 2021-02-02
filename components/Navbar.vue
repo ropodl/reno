9c3d697674ee4196033c78695c71ade6da03ac1e
Saroj Poudel <sarox14@gmail.com>
2021-02-02 18:55:06 +0545

    Initial Commit

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -2,8 +2,43 @@
   <v-app-bar absolute color="transparent" height="70" elevation="0">
     <v-container>
       <v-row class="d-flex align-center mt-10">
-        <span class="font-weight-bold text-h4 white--text">Reno</span>
-
+        <span class="font-weight-bold text-h4 white--text mr-2">Reno</span>
+        <v-dialog v-model="dialog" width="500">
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn icon small v-bind="attrs" v-on="on">
+              <v-icon>info</v-icon>
+            </v-btn>
+          </template>
+          <v-card>
+            <v-card-title
+              >The work done here is based on following link</v-card-title
+            >
+            <v-card-text>
+              <ul>
+                <li>
+                  <a
+                    href="https://www.behance.net/gallery/109349885/Reno-Landing-page?tracking_source=search_projects_recommended%7Cweb%20design%20website"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    >Design Link
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://www.behance.net/mianwaseemabbas"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Designer's Profile
+                  </a>
+                </li>
+              </ul>
+            </v-card-text>
+            <v-card-subtitle>
+              P.S.: Alteration/Modification have been done.
+            </v-card-subtitle>
+          </v-card>
+        </v-dialog>
         <v-spacer></v-spacer>
         <ul class="navbar-items">
           <li v-for="i in nav" :key="i">
@@ -20,6 +55,7 @@
 export default {
   data() {
     return {
+      dialog: false,
       nav: [
         {
           id: "1",
